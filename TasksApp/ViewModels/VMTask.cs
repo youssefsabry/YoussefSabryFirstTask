8594325773962762085558797c8b98dd86554174
youssefsabry <eng.youssefsabry100@gmail.com>
2018-12-27 23:30:18 +0200

    YoussefTask

---
+++ TasksApp/ViewModels/VMTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksApp.ViewModels
+{
+    public class VMTask
+    {
+        private List<Task> tasksList;
+        public List<Task> TasksList
+        {
+            get { return tasksList; }
+            set
+            {
+                if (tasksList == value) return;
+                tasksList = value;
+                RaisePropertyChanged("TasksList");
+            }
+        }
+
+        public VMTask()
+        {
+            TasksList = DataSource.GetTasks();
+        }
+
+        public List<Task> addTask(string name, DateTime date)
+        {
+            TasksList = DataSource.GetTasks();
+            int id = TasksList.Count + 1;
+            TasksList.Add(new Task(id, name+"_"+id.ToString(), date));
+            return TasksList;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
--- TasksApp/Views/MainWindow.xaml.cs
+++ TasksApp/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using TasksApp.ViewModels;
 
 namespace TasksApp
 {
@@ -7,9 +11,49 @@ namespace TasksApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public VMTask VM;
         public MainWindow()
         {
             InitializeComponent();
+            VM = new VMTask();
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(DeadlineDayTbx.Text) <= 31 && int.Parse(DeadlineMonthTbx.Text) <= 12)
+            {
+                DateTime date = Convert.ToDateTime(DeadlineMonthTbx.Text + "/" + DeadlineDayTbx.Text + "/" + DeadlineYearTbx.Text);
+                if (NameTbx.Text != null && date != null)
+                {
+                    if (date >= DateTime.Now)
+                    {
+                        var listTask = VM.addTask(NameTbx.Text, date);
+                        DataLstBx.ItemsSource = null;
+                        DataLstBx.ItemsSource = listTask;
+                        NameTbx.Text = "";
+                        DeadlineDayTbx.Text = "";
+                        DeadlineMonthTbx.Text = "";
+                        DeadlineYearTbx.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter date greater than or equal date of today ");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter Name & Deadline");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid Day & Month");
+            }
         }
     }
 }
